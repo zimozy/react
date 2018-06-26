4a05470a42bd6d07ebf16cc431be96b69b37e93e
Tim <eafwas@gmail.com>
2018-06-25 22:30:57 -0400

    completed assignment

---
+++ reduxcrashcourse/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
